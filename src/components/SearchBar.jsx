fd1a34597f4138af0491bbe71eed7f3e6fc03c8f
Hugoe-Emannuel <85767023+Hugoe-Emannuel@users.noreply.github.com>
2021-11-08 20:29:56 -0300

    requisito 15

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,19 +8,17 @@ function SearchBar() {
     first: false,
   });
 
-  const filtered = async () => {
+  const filteredFood = async () => {
     const { name, ingredients, first } = filters;
     if (name === true) {
       const fetchApi = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`);
       const response = await fetchApi.json();
       setSearchInput('');
-      console.log(response);
       return response;
     } if (ingredients === true) {
       const fetchApi = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`);
       const response = await fetchApi.json();
       setSearchInput('');
-      console.log(response);
       return response;
     } if (searchInput.length > 1 && first === true) {
       return global.alert('Sua busca deve conter somente 1 (um) caracter');
@@ -28,7 +26,28 @@ function SearchBar() {
       const fetchApi = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchInput}`);
       const response = await fetchApi.json();
       setSearchInput('');
-      console.log(response);
+      return response;
+    }
+  };
+
+  const filterDrink = async () => {
+    const { name, ingredients, first } = filters;
+    if (name === true) {
+      const fetchApi = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}`);
+      const response = await fetchApi.json();
+      setSearchInput('');
+      return response;
+    } if (ingredients === true) {
+      const fetchApi = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchInput}`);
+      const response = await fetchApi.json();
+      setSearchInput('');
+      return response;
+    } if (searchInput.length > 1 && first === true) {
+      return global.alert('Sua busca deve conter somente 1 (um) caracter');
+    } if (first === true) {
+      const fetchApi = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${searchInput}`);
+      const response = await fetchApi.json();
+      setSearchInput('');
       return response;
     }
   };
@@ -82,7 +101,10 @@ function SearchBar() {
       <button
         data-testid="exec-search-btn"
         type="button"
-        onClick={ () => filtered() }
+        onClick={
+          window.location.pathname.includes('comidas')
+            ? filteredFood : filterDrink
+        }
       >
         buscar
       </button>
