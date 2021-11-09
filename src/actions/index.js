f0c807f5f667328ec56872562d19b8f847ecc6a8
Victor Veloso <victorveloso13@gmail.com>
2021-11-09 19:23:44 -0300

    Refatora as actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,41 @@
 // export const GET_USER_LS_DATA = 'GET_LS_DATA';
 export const SAVE_USER_LS_DATA = 'SAVE_USER_LS_DATA';
+export const SAVE_RECIPES_LS_DATA = 'SAVE_RECIPES_LS_DATA';
 const userLsKeys = ['user', 'mealsToken', 'cocktailsToken'];
+const recipesLsKeys = ['doneRecipes', 'favoriteRecipes', 'inProgressRecipes'];
 
-export const getUserLsData = () => {
-  const data = userLsKeys.reduce((acc, key) => ({
-    ...acc, [key]: JSON.parse(localStorage.getItem(key)),
-  }), {});
+const getLsData = (keys) => keys.reduce((data, key) => ({
+  ...data, [key]: JSON.parse(localStorage.getItem(key)),
+}), {});
+
+export const getUserLsData = () => ({
+  type: SAVE_USER_LS_DATA,
+  data: getLsData(userLsKeys),
+});
+
+export const getRecipesLsData = () => ({
+  type: SAVE_RECIPES_LS_DATA,
+  data: getLsData(recipesLsKeys),
+});
+
+const saveLsData = (keys, data) => {
+  keys.forEach((key) => {
+    localStorage.setItem(key, JSON.stringify(data[key]));
+  });
+};
+
+export const saveUserLsData = (data) => {
+  saveLsData(userLsKeys, data);
   return {
     type: SAVE_USER_LS_DATA,
     data,
   };
 };
 
-export const saveUserLsData = (data) => {
-  userLsKeys.forEach((key) => {
-    localStorage.setItem(key, JSON.stringify(data[key]));
-  });
+export const saveRecipesLsData = (data) => {
+  saveLsData(recipesLsKeys, data);
   return {
-    type: SAVE_USER_LS_DATA,
+    type: SAVE_RECIPES_LS_DATA,
     data,
   };
 };
