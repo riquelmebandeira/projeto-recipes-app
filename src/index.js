1869dc9ae547ec5ff3d3546f4ac1205194e35bd9
Victor Veloso <victorveloso13@gmail.com>
2021-11-09 19:35:30 -0300

    Corrige importaçao do Provider

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import Provider from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
