83462f6089eadcb58be64e3927af860318f5b87f
Victor Veloso <victorveloso13@gmail.com>
2021-11-10 15:02:38 -0300

    Nova refatoraçao nas actions

--- src/reducer/recipesReducer.js
+++ src/reducer/recipesReducer.js
@@ -1,3 +1,5 @@
+import { SAVE_RECIPES_LS_DATA } from '../actions';
+
 const INITIAL_STATE = {
   doneRecipes: [],
   favoriteRecipes: [],
@@ -9,6 +11,11 @@ const INITIAL_STATE = {
 
 const recipesReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case SAVE_RECIPES_LS_DATA:
+    return {
+      ...state,
+      ...action.data,
+    };
   default: return state;
   }
 };
