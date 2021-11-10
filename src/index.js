b1f3d50ba769e6461b6d9417e1aa7668bb942a0d
Jonathan Ferreira <j0n4t@hotmail.com>
2021-11-10 15:58:32 -0300

    Geral e Redux - Reorganização de arquivos e adicionado redux para o render dos testes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './redux/store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
