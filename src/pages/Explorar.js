d264469632a8452df187737bc0652d22d10f4966
Victor Veloso <victorveloso13@gmail.com>
2021-11-11 17:23:31 -0300

    Adiciona os botoes que redirecionam para outra pagina

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Explorar() {
+  const history = useHistory();
+
+  const handleClick = () => {
+    const click = ({ target: { name } }) => {
+      history.push(`/explorar/${name}`);
+    };
+    return click();
+  };
+
   return (
     <section>
       <Header title="Explorar" showSearchBtn={ false } />
+      <button
+        type="button"
+        name="comidas"
+        onClick={ handleClick }
+        data-testid="explore-food"
+      >
+        Explorar Comidas
+      </button>
+      <button
+        type="button"
+        name="bebidas"
+        onClick={ handleClick }
+        data-testid="explore-drinks"
+      >
+        Explorar bebidas
+      </button>
       <Footer />
     </section>
   );
