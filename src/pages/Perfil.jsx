955a4d74d6c271455d6272c7265c6f8a59b5c284
Hugoe-Emannuel <85767023+Hugoe-Emannuel@users.noreply.github.com>
2021-11-16 16:36:31 -0300

    requisito 82

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -4,9 +4,16 @@ import Header from '../components/Header';
 
 export default function Perfil() {
   return (
-    <section>
-      <Header title="Perfil" showSearchBtn={ false } />
-      <Footer />
-    </section>
+    <>
+      <main />
+      <h3 data-testid="profile-email">email</h3>
+      <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+      <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
+      <button type="button" data-testid="profile-logout-btn">sair</button>
+      <section>
+        <Header title="Perfil" showSearchBtn={ false } />
+        <Footer />
+      </section>
+    </>
   );
 }
