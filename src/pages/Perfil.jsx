50e0c9324a17594efaf5437adb3bda478622bf98
Hugoe-Emannuel <85767023+Hugoe-Emannuel@users.noreply.github.com>
2021-11-16 17:17:57 -0300

    requisito 83

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { getUserLsData } from '../redux/actions';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Perfil() {
+  const user = getUserLsData();
   return (
     <>
       <main />
-      <h3 data-testid="profile-email">email</h3>
+      <h3 data-testid="profile-email">{ user.data.user.email }</h3>
       <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
       <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
       <button type="button" data-testid="profile-logout-btn">sair</button>
