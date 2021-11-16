232366f17e0bd8b3d032dc736471e560a0acca58
Victor Veloso <victorveloso13@gmail.com>
2021-11-16 16:53:49 -0300

    Adiciona os botoes

--- src/pages/ExplorarBebidas.jsx
+++ src/pages/ExplorarBebidas.jsx
@@ -6,6 +6,18 @@ export default function ExplorarBebidas() {
   return (
     <section>
       <Header title="Explorar Bebidas" showSearchBtn={ false } />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+      >
+        Por Ingredientes
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </section>
   );
--- src/pages/ExplorarComidas.jsx
+++ src/pages/ExplorarComidas.jsx
@@ -6,6 +6,24 @@ export default function ExplorarComidas() {
   return (
     <section>
       <Header title="Explorar Comidas" showSearchBtn={ false } />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+      >
+        Por Ingredientes
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-area"
+      >
+        Por Local de Origem
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </section>
   );
