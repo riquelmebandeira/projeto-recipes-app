310716e342363fa7613d5af8042860fc17a98b10
Victor Veloso <victorveloso13@gmail.com>
2021-11-16 17:38:49 -0300

    Adiciona redirecionamento ao clicar nos botoes Por Ingredientes e Por Local de Origem

--- src/pages/ExplorarBebidas.jsx
+++ src/pages/ExplorarBebidas.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function ExplorarBebidas() {
+  const history = useHistory();
+
+  const clickIngredients = () => {
+    history.push('/explorar/bebidas/ingredientes');
+  };
+
   return (
     <section>
       <Header title="Explorar Bebidas" showSearchBtn={ false } />
       <button
         type="button"
         data-testid="explore-by-ingredient"
+        onClick={ clickIngredients }
       >
         Por Ingredientes
       </button>
--- src/pages/ExplorarComidas.jsx
+++ src/pages/ExplorarComidas.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function ExplorarComidas() {
+  const history = useHistory();
+
+  const clickIngredients = () => {
+    history.push('/explorar/comidas/ingredientes');
+  };
+
+  const clickArea = () => {
+    history.push('/explorar/comidas/area');
+  };
+
   return (
     <section>
       <Header title="Explorar Comidas" showSearchBtn={ false } />
       <button
         type="button"
         data-testid="explore-by-ingredient"
+        onClick={ clickIngredients }
       >
         Por Ingredientes
       </button>
       <button
         type="button"
         data-testid="explore-by-area"
+        onClick={ clickArea }
       >
         Por Local de Origem
       </button>
