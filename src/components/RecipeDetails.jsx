f2e398f107262973c10ce7067c16ffb27c2366ad
Riquelme <riquelmebandeira1@outlook.com>
2021-11-18 00:15:55 -0300

    Refatoraçao do RecipeDetails

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,37 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import IngredientsList from './IngredientsList';
 import StartRecipeBtn from './StartRecipeBtn';
 import RecommendationsList from './RecommendationsList';
 import FavoriteBtn from './FavoriteBtn';
 import ShareBtn from './ShareBtn';
-import { fetchRecipeById, fetchRecommendedRecipes,
-  treatVideoUrl, isMeal } from '../utils/DetailsPage';
+import { treatVideoUrl, isMeal } from '../utils/DetailsPage';
 import '../styles/detailsPage.css';
 
-export default function RecipeDetails() {
-  const [recipeInfo, setRecipeInfo] = useState(false);
-  const [recommendations, setRecommendations] = useState();
+export default function RecipeDetails(props) {
+  const { category, alcoholicOrNot, name, image, ingredients,
+    measures, recommendations, instructions, url } = props;
 
-  useEffect(() => {
-    (async () => {
-      setRecommendations(await fetchRecommendedRecipes());
-      setRecipeInfo(await fetchRecipeById());
-    })();
-  }, []);
-
-  const { thumbnail, title, category, instructions,
-    url, ingredients, measures } = recipeInfo;
-
-  return !recipeInfo ? <p>Carregando...</p> : (
+  return (
     <>
       <header>
-        <img src={ thumbnail } alt="Foto da receita" data-testid="recipe-photo" />
+        <img src={ image } alt="Foto da receita" data-testid="recipe-photo" />
       </header>
       <main>
         <section className="container">
           <div className="info-container">
-            <h1 data-testid="recipe-title">{title}</h1>
-            <p data-testid="recipe-category">{category}</p>
+            <h1 data-testid="recipe-title">{name}</h1>
+            <p data-testid="recipe-category">{ isMeal ? category : alcoholicOrNot }</p>
           </div>
           <div className="input-container">
             <FavoriteBtn />
@@ -65,3 +55,13 @@ export default function RecipeDetails() {
     </>
   );
 }
+
+RecipeDetails.propTypes = {
+  category: PropTypes.string,
+  alcoholicOrNot: PropTypes.string,
+  name: PropTypes.string,
+  image: PropTypes.string,
+  ingredients: PropTypes.arrayOf(PropTypes.string),
+  measures: PropTypes.arrayOf(PropTypes.string),
+  recommendations: PropTypes.arrayOf(PropTypes.object),
+}.isRequired;
--- src/pages/BebidasDetalhes.jsx
+++ src/pages/BebidasDetalhes.jsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Loading from '../components/Loading';
 import RecipeDetails from '../components/RecipeDetails';
+import { fetchRecipeById, fetchRecommendedRecipes } from '../utils/DetailsPage';
 
-export default function BebidasDetalhes() {
-  return <RecipeDetails />;
+export default function ComidasDetalhes() {
+  const [recipeInfo, setRecipeInfo] = useState();
+  const [recommendations, setRecommendations] = useState();
+
+  useEffect(() => {
+    (async () => {
+      setRecommendations(await fetchRecommendedRecipes());
+      setRecipeInfo(await fetchRecipeById());
+    })();
+  }, []);
+
+  return !recipeInfo ? <Loading /> : (<RecipeDetails
+    id={ recipeInfo.idDrink }
+    type="comidas"
+    area={ recipeInfo.strArea }
+    category={ recipeInfo.strCategory }
+    alcoholicOrNot={ recipeInfo.strAlcoholic }
+    name={ recipeInfo.strDrink }
+    image={ recipeInfo.strDrinkThumb }
+    ingredients={ recipeInfo.ingredients }
+    measures={ recipeInfo.measures }
+    recommendations={ recommendations }
+    instructions={ recipeInfo.strInstructions }
+    url=""
+  />);
 }
