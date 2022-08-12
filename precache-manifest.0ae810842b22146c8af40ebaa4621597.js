1a52fdd2ab7ecac11cde05842ce5793bca74a9ec
riquelme <riquelmebandeira1@outlook.com>
2022-08-12 05:14:21 -0300

    Updates

---
+++ precache-manifest.0ae810842b22146c8af40ebaa4621597.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448888a871ffa37819eb77e82f48e255",
+    "url": "/projeto-recipes-app/index.html"
+  },
+  {
+    "revision": "fb921dc762209bfc6c4a",
+    "url": "/projeto-recipes-app/static/css/main.d7619770.chunk.css"
+  },
+  {
+    "revision": "60e0299a01446d8cba95",
+    "url": "/projeto-recipes-app/static/js/2.4579a4c6.chunk.js"
+  },
+  {
+    "revision": "2faa44b8b872a8d482d55095973ed97b",
+    "url": "/projeto-recipes-app/static/js/2.4579a4c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb921dc762209bfc6c4a",
+    "url": "/projeto-recipes-app/static/js/main.8400667f.chunk.js"
+  },
+  {
+    "revision": "9fe4ee6aadbad6d8d15e",
+    "url": "/projeto-recipes-app/static/js/runtime-main.9e2bc4de.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/projeto-recipes-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/projeto-recipes-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/projeto-recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/projeto-recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/projeto-recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/projeto-recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/projeto-recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/projeto-recipes-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
